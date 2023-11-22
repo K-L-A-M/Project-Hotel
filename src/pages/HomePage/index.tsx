fe3671ec42a1b639799c8f847b5433c97d9dd433
Anderson Verrone Funicello <107007506+AndersonVerrone@users.noreply.github.com>
2023-11-22 00:05:39 -0300

    update: atualizar home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,10 @@
+import { InputSearchHome } from "../../components/InputSearchHome"
+import { StyledHomePage } from "./styles"
+
 export const HomePage = () => {
     return (
-        <>
-        </>
+        <StyledHomePage>
+            <InputSearchHome/>
+        </StyledHomePage>
     )
 }
---
+++ src/pages/HomePage/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledHomePage = styled.main`
+    width: 100%;
+    height: 100vh;
+`
